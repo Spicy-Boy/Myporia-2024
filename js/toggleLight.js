17f0d99de9bf21a8028f45b0b173b8d9be5f0ebe
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2024-02-12 16:40:08 -0500

    Figured out how to reset JS assigned style

--- js/toggleLight.js
+++ js/toggleLight.js
@@ -1,27 +1,27 @@
 
+// let isLightToggled = false;
 let isLightToggled = false;
-// ^^^ this needs to be set by cookies somehow
 
-function toggleLightMode () {
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("lightButton").addEventListener("click", toggleLightMode);
+});
+
+setTheme();
+
+function setTheme ()
+{
+    console.log("Setting theme!");
+
     if (isLightToggled)
     {
-        isLightToggled = false;
-    }
-    else {
-        isLightToggled = true;
-    }
-
-    if (isLightToggled)
-    {
-        //content-entry marks individual posts
-        //vvv sets background color to white
+        // vvv grabs all content-entry divs and sets background white
         let contentEntries = document.getElementsByClassName("content-entry");
 
         for (var i = 0; i < contentEntries.length; i++) 
         {
             contentEntries[i].style.backgroundColor = "white";
         }
-
+        
         //vvv grabs all the text tags on the page to change text color to black
         let textElements = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, span, div, a, li");
 
@@ -30,16 +30,33 @@ function toggleLightMode () {
             textElements[i].style.color = "black";
         }
 
-
+        // vvv sets background to pitch black
         let domBody = document.body;
         domBody.style.backgroundColor = "black";
-
-        console.log("TOGGLED LIGHT MODE");
     }
+    else {
+        // vvv resets all JS assigned style
+        // Get all elements on the page
+        var allElements = document.querySelectorAll('*');
+
+        // Iterate through each element and remove inline styles
+        allElements.forEach(function (element) {
+            element.style = '';
+        });
+    }
+
+
+
 }
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("lightButton").addEventListener("click", toggleLightMode);
-});
+
+function toggleLightMode () {
+    isLightToggled = !isLightToggled;
+
+    console.log("Light mode toggled? "+isLightToggled);
+
+    setTheme();
+}
+
 
 // document.getElementById("lightButton").addEventListener("click", toggleLightMode);
 
