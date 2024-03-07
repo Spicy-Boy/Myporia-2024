1e12e3a5ecbe49340ba26d6394da4fda66b77ae6
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2024-03-07 12:22:29 -0500

    Quick tweak, gave content-single its own container

--- content-single.php
+++ content-single.php
@@ -1,6 +1,6 @@
 <!-- this gets displayed on individual post pages -->
 <!-- called from single.php! -->
-<article class="content-entry content-entry-single">
+<article class="content-entry-single">
 
     <div class="content-title-wrapper">
         <div class="content-title">
